4d5e914aefac60c9a4e17e585859110cf1ec018c
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-03-16 12:05:02 -0300

    Rolândia:16.03.2022_12:04hs

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,93 +1,110 @@
 <template>
-  <!-- Aqui no componente vue, somente html e javascript -->
-  <!-- Como os componentes são executador do lado do front,
-     não tem acesso a nenhum recursos php -->
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Login (Componente Vue)</div>
-          <div class="card-body">
-            <form method="POST" action="">
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right"
-                  >E-mail</label
-                >
+    <!-- Aqui no componente vue, somente html e javascript -->
+    <!-- Como os componentes são executador do lado do front,
+         não tem acesso a nenhum recursos php ou laravel -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Login (Componente Vue)</div>
+                    <div class="card-body">
+                        <form method="POST" action>
+                            <input
+                                type="hidden"
+                                name="_token"
+                                :value="token_csrf"
+                            />
 
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value=""
-                    required
-                    autocomplete="email"
-                    autofocus
-                  />
+                            <div class="form-group row">
+                                <label
+                                    for="email"
+                                    class="col-md-4 col-form-label text-md-right"
+                                    >E-mail</label
+                                >
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        class="form-control"
+                                        name="email"
+                                        value
+                                        required
+                                        autocomplete="email"
+                                        autofocus
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label
+                                    for="password"
+                                    class="col-md-4 col-form-label text-md-right"
+                                    >Senha</label
+                                >
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="password"
+                                        type="password"
+                                        class="form-control"
+                                        name="password"
+                                        required
+                                        autocomplete="current-password"
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input
+                                            class="form-check-input"
+                                            type="checkbox"
+                                            name="remember"
+                                            id="remember"
+                                        />
+
+                                        <label
+                                            class="form-check-label"
+                                            for="remember"
+                                            >Mantenha-me conectado</label
+                                        >
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button
+                                        type="submit"
+                                        class="btn btn-primary"
+                                    >
+                                        Login
+                                    </button>
+
+                                    <a class="btn btn-link" href="#"
+                                        >Esqueci a senha</a
+                                    >
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-              </div>
-
-              <div class="form-group row">
-                <label
-                  for="password"
-                  class="col-md-4 col-form-label text-md-right"
-                  >Senha</label
-                >
-
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    autocomplete="current-password"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-6 offset-md-4">
-                  <div class="form-check">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      name="remember"
-                      id="remember"
-                    />
-
-                    <label class="form-check-label" for="remember">
-                      Mantenha-me conectado
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Login</button>
-
-                  <a
-                    class="btn btn-link"
-                    href="#"
-                  >
-                    Esqueci a senha
-                  </a>
-                </div>
-              </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
-
 <script>
+/* Parte dedicada para codificação javascript */
+/* Agora, após ter exportado este objeto literal, estas propriedades
+   estarão disponíveis para o template e para este script também. */
+/* Objeto padrão de exportação para configuração do vueJS, no front */
+export default {
+    /* Aqui, as props são sempre convertidas para letras minúsculas */
+    props: ["token_csrf"],
+};
 </script>
 
-
-<style>
-</style>
+<style></style>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 {{-- Aqui no blade, tem acesso a recursos php --}}
+{{-- Aqui tem acessos aos helpers do laravel --}}
+{{-- Aqui pode recuperar variáveis que são encaminhas dos controllers para as views --}}
 @section('content')
-    <login-component></login-component>
+    {{-- Instância do componente "Login.vue" --}}
+    {{-- Enviando as "props" daqui, para o componente vue --}}
+    <login-component token_csrf="{{ @csrf_token() }}"></login-component>
 @endsection
