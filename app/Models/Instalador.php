448fa9c7766aeff6481109c0b33687706127c3ee
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-04-20 17:48:17 -0300

    Rolândia:20.04.2022_17:48hs

--- app/Models/Instalador.php
+++ app/Models/Instalador.php
@@ -6,27 +6,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Instalador extends Model
-{
+ {
     //use SoftDeletes;
     use HasFactory;
 
     /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
+    * The database table used by the model.
+    *
+    * @var string
+    */
     protected $table = 'instaladores';
 
     /**
-     * The database primary key value.
-     *
-     * @var string
-     */
+    * The database primary key value.
+    *
+    * @var string
+    */
     protected $primaryKey = 'id';
 
-    /* Por questão de segurança, o "fillable" é para dizer quais são
-       os únicos campos que serão aceitos pelo model. O usuário poderia
-       tentar inspecionar e injetar algum campo a mais, lá no front. */
+    /* Por questão de segurança, o 'fillable' é para dizer quais são
+    os únicos campos que serão aceitos pelo model. O usuário poderia
+    tentar inspecionar e injetar algum campo a mais, lá no front. */
     /* Modo massivo */
     protected $fillable = [
         'nome', 'cpf', 'rg', 'celular', 'telefone', 'email', 'cep', 'bairro', 'cidade',
@@ -34,7 +34,7 @@ class Instalador extends Model
     ];
 
     public function rules()
-    {
+ {
         return [
             //'nome' => 'required|unique:antenas,nome, ' . $this->id . '|min:3',
             //'imagem' => 'required|file|mimes:png,docx,xlsx,pdf,ppt,jpeg,mp3',
@@ -54,21 +54,22 @@ class Instalador extends Model
             //'dataNascimento' => 'required'
         ];
 
-        /* A validação "unique" tem 3 parâmetros:
-           1) Qual tabela
-           2) Nome da coluna que será pesquisada nesta tabela
-           3) id do registro que será desconsiderado na pesquisa */
+        /* A validação 'unique' tem 3 parâmetros:
+        1 ) Qual tabela
+        2 ) Nome da coluna que será pesquisada nesta tabela
+        3 ) id do registro que será desconsiderado na pesquisa */
     }
 
     /* Relacionamentos: */
+
     public function instalacoes()
-    {
+ {
         /* Um instalador possui várias instalações */
-        return $this->hasMany('App\Models\Instalacao');
+        return $this->hasMany( 'App\Models\Instalacao' );
     }
 
     public function feedback()
-    {
+ {
         return  [
             //'required' => 'O campo :attribute é obrigatório',
             //'imagem.mimes' => 'Somente arquivos do tipo png,docx,xlsx,pdf,ppt,jpeg,mp3',
